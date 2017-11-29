41321ccbbb7b88f48fc3ce188a339d2760cc44e2
Mikko Piuhola <mikko.piuhola@gmail.com>
2017-11-29 19:17:52 +0200

    Add more resources to example.tf

--- example.tf
+++ example.tf
@@ -2,7 +2,25 @@ provider "aws" {
   region = "eu-central-1"
 }
 
+resource "aws_s3_bucket" "example" {
+  # NOTE: S3 bucket names must be unique across _all_ AWS accounts, so
+  # this name must be changed before applying this example to avoid naming
+  # conflicts.
+  bucket = "mipiuhol-terraform-example-bucket"
+  acl    = "private"
+}
+
 resource "aws_instance" "example" {
-  ami = "ami-bf2ba8d0"
+  ami = "ami-df8406b0"
   instance_type = "t2.micro"
+
+  depends_on = ["aws_s3_bucket.example"]
+
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
+  }
+}
+
+resource "aws_eip" "ip" {
+  instance = "${aws_instance.example.id}"
 }
